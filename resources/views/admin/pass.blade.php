b8578b92423931340219eafd1d814fd496caeda4
zhuwen <zhuwen975@pingan.com.cn>
2017-04-15 02:25:29 +0800

    update

--- resources/views/admin/pass.blade.php
+++ resources/views/admin/pass.blade.php
@@ -13,13 +13,15 @@
 <div class="result_wrap">
     <div class="result_title">
         <h3>修改密码</h3>
+
         @if(count($errors)>0)
             <div class="mark">
-                @foreach(@errors->all() as $error)
+                @foreach($errors->all() as $error)
                     <p>{{$error}}</p>
                 @endforeach
             </div>
         @endif
+
     </div>
 </div>
 <!--结果集标题与导航组件 结束-->
