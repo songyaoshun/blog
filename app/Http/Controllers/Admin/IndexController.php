2937e4b8272acf75b2fe5a5164650845cd5f2380
zhuwen <zhuwen975@pingan.com.cn>
2017-04-15 06:59:10 +0800

    update

--- app/Http/Controllers/Admin/IndexController.php
+++ app/Http/Controllers/Admin/IndexController.php
@@ -49,7 +49,7 @@ class IndexController extends CommonController
                 if ($input['password_o']==$_password){
                     $user->user_pass = Crypt::encrypt($input['password']);
                     $user->update();
-
+                    return back()->with('errors','密码修改成功');
                 }else{
                     return back()->with('errors','原密码错误');
                 }
--- resources/views/admin/pass.blade.php
+++ resources/views/admin/pass.blade.php
@@ -16,9 +16,13 @@
 
         @if(count($errors)>0)
             <div class="mark">
-                @foreach($errors->all() as $error)
-                    <p>{{$error}}</p>
-                @endforeach
+                @if(is_object($errors))
+                    @foreach($errors->all() as $error)
+                        <p>{{$error}}</p>
+                    @endforeach
+                @else
+                    <p>{{$errors}}</p>
+                @endif
             </div>
         @endif
 
