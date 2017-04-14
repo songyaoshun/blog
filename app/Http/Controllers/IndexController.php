5d762a47fc2c21172c6108912b0af82e7c2157a0
zhuwen <zhuwen975@pingan.com.cn>
2017-04-14 18:03:35 +0800

    2

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -5,11 +5,13 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
+use Illuminate\Support\Facades\DB;
 
 class IndexController extends Controller
 {
     public function index()
     {
-        echo 123;
+        $pdo=DB::connection()->getPdo();
+        dd($pdo);
     }
 }
