da2970389ad3f3fe9b85982648ecadbfdd1412f7
zhuwen <zhuwen975@pingan.com.cn>
2017-04-15 02:22:55 +0800

    update

--- app/Http/Controllers/Admin/IndexController.php
+++ app/Http/Controllers/Admin/IndexController.php
@@ -3,10 +3,12 @@
 namespace App\Http\Controllers\Admin;
 
 
+use App\Http\Model\User;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Crypt;
 use Illuminate\Support\Facades\Input;
 use Illuminate\Support\Facades\Validator;
 
@@ -29,6 +31,7 @@ class IndexController extends CommonController
     {
 
         if ($input=Input::all()){
+            dd($input);
             $rules=[
                 'password'=>'required|between:6,20|confirmed',
             ];
@@ -36,14 +39,24 @@ class IndexController extends CommonController
             $message=[
                 'password.required'=>'新密码不能为空！',
                 'password.between'=>'新密码必须是6到20位之间！',
+                'password.confirmed'=>'新旧密码不一致！',
             ];
 
             $validator=Validator::make($input,$rules,$message);
 
             if($validator->passes()){
-                echo 'Yes';
+                $user = User::first();
+                $_password=Crypt::decrypt($user->user_passwd);
+                if ($input['password_o']==$_password){
+                    $user->user_pass = Crypt::encrypt($input['password_o']);
+                    $user->update();
+
+                }else{
+                    return back()->with('errors','原密码错误');
+                }
+
             }else{
-                 dd($validator->errors()->all());
+                return back()->withErrors($validator);
             }
 
         }else{
--- resources/views/admin/pass.blade.php
+++ resources/views/admin/pass.blade.php
@@ -13,13 +13,23 @@
 <div class="result_wrap">
     <div class="result_title">
         <h3>修改密码</h3>
+        @if(count($errors)>0)
+            <div class="mark">
+                @foreach(@errors->all() as $error)
+                    <p>{{$error}}</p>
+                @endforeach
+            </div>
+        @endif
     </div>
 </div>
 <!--结果集标题与导航组件 结束-->
 
 <div class="result_wrap">
     <form method="post" onsubmit="return changePass()">
-        <input type="hidden" name="_token" value="X25wGVjFqDXvq7vAUAJTjTAHfX0RhkGufucRdzGh">
+        {{csrf_field()}}
+
+
+
         <table class="add_tab">
             <tbody>
             <tr>
