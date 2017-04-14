467c18fa8fc79087c73e0530cf2ce28319670326
zhuwen <zhuwen975@pingan.com.cn>
2017-04-15 01:16:11 +0800

    update

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,13 +1,13 @@
 <?php
 
 
-Route::group(['middleware' => ['web'],'prefix'=>'admin','namespace'=>'admin'], function () {
+Route::group(['middleware' => ['web'],'prefix'=>'admin','namespace'=>'Admin'], function () {
     Route::any('login','LoginController@login');
     Route::get('code','LoginController@code');
     Route::get('getcode','LoginController@getcode');
 });
 
-Route::group(['middleware' => ['web','admin.login'],'prefix'=>'admin','namespace'=>'admin'],function (){
+Route::group(['middleware' => ['web','admin.login'],'prefix'=>'admin','namespace'=>'Admin'],function (){
     Route::get('index','IndexController@index');
     Route::get('info','IndexController@info');
     Route::get('quit','LoginController@quit');
