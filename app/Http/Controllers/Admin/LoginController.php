c49bf6326465329cc6d63d18a1f712397451bf0d
zhuwen <zhuwen975@pingan.com.cn>
2017-04-14 22:57:39 +0800

    update

--- app/Http/Controllers/Admin/LoginController.php
+++ app/Http/Controllers/Admin/LoginController.php
@@ -19,7 +19,7 @@ class LoginController extends CommonController
             $code=new \Code;
             $_code=$code->get();
 
-            if($input['code']!=$_code){
+            if(strtoupper($input['code'])!=$_code){
                return back()->with('msg','验证码错误');
             }else{
                 echo 123;
