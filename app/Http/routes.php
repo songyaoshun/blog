b49137c581a33ade0d647704414ff7211b1dc998
zhuwen <zhuwen975@pingan.com.cn>
2017-04-15 01:14:59 +0800

    update

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,47 +1,14 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Routes File
-|--------------------------------------------------------------------------
-|
-| Here is where you will register all of the routes in an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the controller to call when that URI is requested.
-|
-*/
 
-Route::get('/', function () {
-    echo 123;exit;
-    return view('welcome');
+Route::group(['middleware' => ['web'],'prefix'=>'admin','namespace'=>'admin'], function () {
+    Route::any('login','LoginController@login');
+    Route::get('code','LoginController@code');
+    Route::get('getcode','LoginController@getcode');
 });
 
-
-
-
-Route::get('admin/code','Admin\LoginController@code');
-Route::get('admin/getcode','Admin\LoginController@getcode');
-Route::get('admin/quit','Admin\LoginController@quit');
-
-
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| This route group applies the "web" middleware group to every route
-| it contains. The "web" middleware group is defined in your HTTP
-| kernel and includes session state, CSRF protection, and more.
-|
-*/
-
-Route::group(['middleware' => ['web']], function () {
-    Route::any('admin/login','Admin\LoginController@login');
-
-});
-
-Route::group(['middleware' => ['admin.login']],function (){
-    Route::get('admin/index','Admin\IndexController@index');
-    Route::get('admin/info','Admin\IndexController@info');
-
+Route::group(['middleware' => ['web','admin.login'],'prefix'=>'admin','namespace'=>'admin'],function (){
+    Route::get('index','IndexController@index');
+    Route::get('info','IndexController@info');
+    Route::get('quit','LoginController@quit');
 });
